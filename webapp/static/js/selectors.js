6ca9b209ae209013e6adcdefc1bbc754b300b11a
Ilya Sosnovsky <yakrewedko@ya.ru>
2019-03-30 03:24:56 +0300

    Add js

--- webapp/static/js/selectors.js
+++ webapp/static/js/selectors.js
@@ -1,5 +1,19 @@
 $(document).ready(function () {
-    $("car-brand-field").change(function () {
-        $.get('get_all_brand_models/'.value);
+    $("#car-brand-field").on("change", function () {
+        $.getJSON('get_all_brand_models/' + $("#car-brand-field").val(), function (data) {
+            var modelField = $("#car-model-field").html("");
+            modelField.append("<option disabled selected >Select model</option>");
+            data.forEach(function(model) {
+               modelField.append("<option value='" +model.id + "'>"+model.name +"</option>");
+            });
+            modelField.removeAttr("disabled");
+        });
+    });
+
+    $("#car-model-field").on("change", function () {
+        $.getJSON('get_all_brand_models/' + $("#car-brand-field").val(), function (data) {
+            $("#car-transmission-field").removeAttr("disabled");
+            $("#car-privod-field").removeAttr("disabled");
+        });
     });
 });
